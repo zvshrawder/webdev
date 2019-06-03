42b786d5006b02f96f0f5006cc40b54b8fae0415
ZVShrawder <zvshrawder@gmail.com>
2019-06-02 23:09:34 -0400

    php7.2-fpm fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ sudo echo “LC_CTYPE=en_US.UTF-8” >> /etc/environment
 
 sudo apt-get update && apt-get upgrade
 sudo apt-get -y -q install nginx
-sudo apt-get install -q -y php7.2 libapache2-mod-php7.2
-sudo apt-get install -q -y php7.2-curl php7.2-gd php7.2-mbstring php7.2-mysql php7.2-xml php7.2-zip php7.2-bz2 php7.2-intl
+sudo apt-get -y -q install php7.2-cli php7.2-fpm php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip php7.2-xml php7.2-zip
 sudo apt-get -q -y install composer
 
 #mariadb 10.2
@@ -42,7 +41,12 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo cp /vagrant/etc/nginx/sites-enabled/zvshrawder.conf /etc/nginx/sites-enabled/zvshrawder.conf
 sudo cp /vagrant/etc/hosts /etc/hosts
 sudo cp /vagrant/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp  /vagrant/etc/php/7.2/fpm/pool.d/www.conf /etc/php/7.2/fpm/pool.d/www.conf
 
 chmod -R 755 /var/log/
 
+sudo chown -R www-data:www-data /var/www/
+sudo chmod -R 775 /var/www/
+
+sudo service php7.2-fpm restart
 sudo service nginx restart
